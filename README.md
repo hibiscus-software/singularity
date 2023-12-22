2cbd8ed4478683d45b6a272589bcdd5c5c662ea9
Cody L. Wellman <cody@codexmicro.systems>
2023-12-22 00:25:58 -0500

    Add "and contributors" to license

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 * Language Support for Java, C++, and Python
 
 ## License
-Copyright © 2023, Codex Microsystems. All rights reserved. This work is licensed
-under the terms of the MIT license which can be found in the root directory of
-this project.
+Copyright © 2023, Codex Microsystems and contributors. All rights reserved.
+This work is licensed under the terms of the MIT license which can be found
+in the root directory of this project.
--- singularity-cpp/src/main/cpp/SwerveDrive.cpp
+++ singularity-cpp/src/main/cpp/SwerveDrive.cpp
@@ -1,7 +1,7 @@
 /*
- * Copyright (C), 2023 Codex Microsystems. Some rights reserved. This work is
- * licensed under the terms of the MIT license which can be found in the
- * root directory of this project.
+ * Copyright (C), 2023 Codex Microsystems and contributors. Some rights
+ * reserved. This work is licensed under the terms of the MIT license
+ * which can be found in the root directory of this project.
  */
 
 #include "singularity/SwerveDrive.hpp"
